f7fcd5b4b2cd4cbceed3b0b018994f11cefe9d9a
Alessandro Mencarini <hello@amencarini.com>
2017-11-14 11:34:44 +0000

    Accept hackney options (#6)
    This also update mock/meck deps, formats code

--- test/mockingbird/httpoison_http_client_test.exs
+++ test/mockingbird/httpoison_http_client_test.exs
@@ -5,16 +5,35 @@ defmodule Mockingbird.HTTPoisonHttpClientTest do
 
   describe "Mockingbird.HTTPoisonHttpClient.call/4" do
     test "the get call accepts a map that will be expanded into the query stirng" do
-      with_mock HTTPoison, [get: fn("http://example.com?test=true", %{}) -> "<html></html>" end] do
+      with_mock HTTPoison, get: fn "http://example.com?test=true", %{}, [] -> "<html></html>" end do
         Mockingbird.HTTPoisonHttpClient.call(:get, "http://example.com", %{test: true})
-        assert called HTTPoison.get("http://example.com?test=true", %{})
+        assert called(HTTPoison.get("http://example.com?test=true", %{}, []))
       end
     end
 
     test "the get call uses the provided querystring" do
-      with_mock HTTPoison, [get: fn("http://example.com?test=true", %{}) -> "<html></html>" end] do
+      with_mock HTTPoison, get: fn "http://example.com?test=true", %{}, [] -> "<html></html>" end do
         Mockingbird.HTTPoisonHttpClient.call(:get, "http://example.com?test=true")
-        assert called HTTPoison.get("http://example.com?test=true", %{})
+        assert called(HTTPoison.get("http://example.com?test=true", %{}, []))
+      end
+    end
+
+    test "the get call uses the provided hackney options" do
+      ssl_options = [ssl: [{:versions, [:"tlsv1.2"]}]]
+
+      with_mock HTTPoison,
+        get: fn "http://example.com?test=true", [], ^ssl_options ->
+          "<html></html>"
+        end do
+        Mockingbird.HTTPoisonHttpClient.call(
+          :get,
+          "http://example.com?test=true",
+          %{},
+          [],
+          ssl_options
+        )
+
+        assert called(HTTPoison.get("http://example.com?test=true", [], ssl_options))
       end
     end
   end
